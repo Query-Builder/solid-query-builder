fcd1ec12c4b6dfce1d229d91bd3c04ba7071ddad
Swapnesh <swapneshsangle1@gmail.com>
2024-11-02 18:46:16 +0530

    fix: update combinator type within query to be string

--- src/types/rule-group.ts
+++ src/types/rule-group.ts
@@ -6,8 +6,8 @@ export type RulesType = (RuleType | RuleGroupType)[];
 export type RuleGroupType = {
   /** UUID v4 */
   id: string;
-  /** Combinator support to be AND or OR */
-  combinator: Combinator;
+  /** selected combinator */
+  combinator: string;
   /** Lock the group to prevent any changes to the group */
   locked?: boolean;
   /** Negate the group to return the opposite result */
